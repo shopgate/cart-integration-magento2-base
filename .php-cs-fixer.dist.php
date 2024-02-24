1cd99e6020c3ebfcc30c930e4c183c51f0ce46fd
Konstantin Kiritsenko <k@componentk.com>
2024-02-24 22:06:51 +0300

    SG-209 CS fixer check update

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,6 @@
 <?php
 
-// For more information visit the Github repository: https://github.com/FriendsOfPHP/PHP-CS-Fixer
+// For more information visit the GitHub repository: https://github.com/FriendsOfPHP/PHP-CS-Fixer
 
 // How to set up PHP-CS-Fixer in PHPStorm:
 // https://hackernoon.com/how-to-configure-phpstorm-to-use-php-cs-fixer-1844991e521f
@@ -14,16 +14,17 @@ $fixers = array(
     'blank_line_after_opening_tag' => true,
     'method_argument_space' => false,
 );
-$finder = Finder::create()
-                ->exclude('vendor')
-                ->exclude('vendors')
-                ->exclude('release')
-                ->in(__DIR__);
+$finder = (new Finder())
+    ->in(__DIR__)
+    ->exclude('tests/node_modules')
+    ->exclude('vendor')
+    ->exclude('vendors')
+    ->exclude('release');
 
 $config = new Config();
 $config->setRiskyAllowed(true)
-       ->setUsingCache(true)
-       ->setRules($fixers)
-       ->setFinder($finder);
+    ->setUsingCache(true)
+    ->setRules($fixers)
+    ->setFinder($finder);
 
 return $config;
